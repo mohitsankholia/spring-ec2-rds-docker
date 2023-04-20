de15a169a8fe584a28e7e8d86244142200ed5a9a
mohitsankholia <sankholia.mohit@gmail.com>
2023-04-20 11:30:41 +0530

    first commit

---
+++ src/main/java/com/mohit/springec2docker/SpringEc2DockerApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.springec2docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEc2DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEc2DockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohit/springec2docker/SpringEc2DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.springec2docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEc2DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
