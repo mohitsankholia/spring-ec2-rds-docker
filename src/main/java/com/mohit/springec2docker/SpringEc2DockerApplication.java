d2bdc2db34fa9c3a0e922620f83a0681f5959b4e
mohitsankholia <sankholia.mohit@gmail.com>
2023-04-20 16:18:47 +0530

    dev: openapi3 added

--- src/main/java/com/mohit/springec2docker/SpringEc2DockerApplication.java
+++ src/main/java/com/mohit/springec2docker/SpringEc2DockerApplication.java
@@ -3,7 +3,11 @@ package com.mohit.springec2docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Practice API", version = "2.0", description = "Spring-Ec2-RDS-Docker"))
 public class SpringEc2DockerApplication {
 
 	public static void main(String[] args) {
